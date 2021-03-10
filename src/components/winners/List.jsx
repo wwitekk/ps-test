ca01850b892a789c2d77ccff441dceb9cd8514e0
Witold Paprotny <witold.paprotny@vattenfall.com>
2021-03-10 23:37:01 +0100

    created issues to be fixed

--- src/components/winners/List.jsx
+++ src/components/winners/List.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const Item = ({ item: { season, driver } }) => (
   <li>
@@ -11,34 +11,31 @@ const Item = ({ item: { season, driver } }) => (
 const List = ({ items }) =>
   items.map((item, idx) => <Item item={item} idx={idx} />)
 
-const sortBySeasonDesc = (a, b) => {
-  const aNum = parseInt(a.season)
-  const bNum = parseInt(b.season)
-  return bNum - aNum
-}
+const handleItems = items => {
+  const result = []
+  // below create code to manipulate items
+  // to get required number of items in requested order
 
-const handleItems = items =>
-  items.sort(sortBySeasonDesc).filter((item, idx) => idx < 15)
+  return result
+}
 
 const ListContainer = () => {
   const [items, setItems] = useState([])
 
   // task 1. Data are not loading - why?
-  useEffect(() => {
-    const url = 'http://ergast.com/api/f1/driverStandings/1.json?limit=300'
-    fetch(url)
-      .then(respone => respone.json())
-      .then(data => data.MRData.StandingsTable.StandingsLists)
-      // task 2. Get 15 latest winners ordered by season (from season 2020 to 2006)
-      .then(handleItems)
-      .then(items =>
-        items.map(item => ({
-          season: item.season,
-          driver: item.DriverStandings[0].Driver
-        }))
-      )
-      .then(items => setItems(items))
-  }, [])
+  const url = 'http://ergast.com/api/f1/driverStandings/1.json?limit=300'
+  fetch(url)
+    .then(respone => respone.json())
+    .then(data => data.MRData.StandingsTable.StandingsLists)
+    // task 2. Update handleItems function to get 15 latest winners ordered by season (from season 2020 to 2006)
+    .then(handleItems)
+    .then(items =>
+      items.map(item => ({
+        season: item.season,
+        driver: item.DriverStandings[0].Driver
+      }))
+    )
+    .then(items => setItems(items))
 
   return <List items={items} />
 }
